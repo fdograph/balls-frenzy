08bde2cfe6de557c8ef4dad42a305a6f622fd377
Fernando Silva <fdograph@gmail.com>
2020-02-26 22:21:43 +0100

    Updates

---
+++ precache-manifest.f077919de521b02bcb44289137ea8504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae747d0e46f988b7be66d37dbdc6014",
+    "url": "/balls-frenzy/index.html"
+  },
+  {
+    "revision": "ce2ed947ec921dd3c5f1",
+    "url": "/balls-frenzy/static/js/2.376d8140.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/balls-frenzy/static/js/2.376d8140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebdfd09062f9f660f09",
+    "url": "/balls-frenzy/static/js/main.5d024d5f.chunk.js"
+  },
+  {
+    "revision": "e1dbf6516849d8eb9509",
+    "url": "/balls-frenzy/static/js/runtime-main.2605f2a6.js"
+  }
+]);
